f4e4be3d47bbd10ed789e4edaf9ba5007c2989e9
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-03-26 16:30:58 +0100

    add description to is_valid_address

--- helpers/bitcoin.py
+++ helpers/bitcoin.py
@@ -1,12 +1,17 @@
 import re
 
-from hashlib import sha256
-
 class bitcoin: 
     def __init__(self) -> None:
         pass 
     
-    def is_valid_address(address: str) -> bool: 
+    def is_valid_address(string: str) -> bool: 
+        """Check if a string is a valid bitcoin address.
+
+        Args:
+            string (str): String to check for an address.
+        Returns:
+            bool: Return if the string contains a valid address.
+        """
         address_detection_regex = "^(1|[13][a-km-zA-HJ-NP-Z0-9]{25,34}|(bc1|[02-9ac-hj-np-z]{1})[a-zA-HJ-NP-Z0-9]{16,100})$"
         
-        return bool(re.match(address_detection_regex, address))
+        return bool(re.match(address_detection_regex, string))
